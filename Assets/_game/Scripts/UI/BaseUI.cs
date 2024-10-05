269f1f0ab0ec18e6793b7cb24930a1a3d89214d2
Dragoons <arnolds.batkovskis@gmail.com>
2024-10-05 12:09:50 +0300

    Added main menu Sound for buttons Dotween Package for tweening

---
+++ Assets/_game/Scripts/UI/BaseUI.cs
@@ -0,0 +1,27 @@
+using _game.Scripts.Extensions;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace _D.Scripts.UI
+{
+    [RequireComponent(typeof(UIDocument))]
+    public class BaseUI : MonoBehaviour
+    {
+        [SerializeField] protected UIDocument uiDocument;
+        [SerializeField] protected StyleSheet[] styleSheets;
+        
+        protected VisualElement root;
+        protected VisualElement container;
+
+        protected virtual void Awake()
+        {
+            uiDocument = GetComponent<UIDocument>();
+            root = uiDocument.rootVisualElement;
+            foreach (var file in styleSheets)
+            {
+                root.styleSheets.Add(file);
+            }
+            container = root.CreateChild("container");
+        }
+    }
+}
